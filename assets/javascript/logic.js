2eb3058a33f64e4b364f024d7f2ec198653095b8
Andrew P <andrewpetersondev@gmail.com>
2019-06-13 23:04:23 -0500

    removed js folder and moved code into a new file

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,4 +1,5 @@
 
+// whos file is this? it was formerly app.js 
 
 // The below code fills in the first row of the table 
 var queryURL = "http://strainapi.evanbusse.com/cpTeJEf/strains/search/all";
@@ -46,3 +47,34 @@ $.ajax({
         }
     }
 })
+
+// this code below was formerly love.js
+
+// api key : 765b72b611msh83e1deab1282842p15aaf5jsn577be8027784
+
+// click button and receive data from api
+$("#love").on("click", function (event) {
+    event.preventDefault();
+    
+    console.log('clicked');
+    
+    var proxy = "https://cors-anywhere.herokuapp.com/";
+    
+        var queryURL = "https://love-calculator.p.rapidapi.com/getPercentage?fname=John&sname=Alice";
+    
+        $.ajax({
+    
+    
+            url: proxy+queryURL,
+            headers: { "X-RapidAPI-Key": "e0e5de8bf8msh31b01dba1b8405fp18c6fejsn08e5f81c5f7c", "X-RapidAPI-Host": "love-calculator.p.rapidapi.com" },
+            method: "GET"
+    
+    
+        }).then(function (response) {
+    
+            console.log(response);
+    
+        })
+    
+    });
+    
